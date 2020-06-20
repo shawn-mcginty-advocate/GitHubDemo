d15264e598c22deefa2a3ad8eca4eefdf32801a2
subsonar704 McGinty <spmcginty1030@gmail.com>
2020-06-20 16:10:45 -0400

    Add project files.

---
+++ GitHubDemo/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace GitHubDemo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
